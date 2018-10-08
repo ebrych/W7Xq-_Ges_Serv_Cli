ba40e8a96369e3dfaaad2d3bad881d42926d4fa4
ebrych <ebrych@gmail.com>
2018-10-07 21:58:08 -0500

    domingo

--- src/app/components/marcacion/marcacion.component.ts
+++ src/app/components/marcacion/marcacion.component.ts
@@ -3,12 +3,14 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { GlobalVariable } from '../../global';
 
+
 @Component({
   selector: 'app-marcacion',
   templateUrl: './marcacion.component.html',
   styles: []
 })
 export class MarcacionComponent implements OnInit {
+  
   constructor(private http:HttpClient,private router:Router) { }
   private baseApiUrl = GlobalVariable.BASE_API_URL;
 
