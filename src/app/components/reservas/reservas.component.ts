d296697b2d15fded643118a6762991f3785d9142
ebrych <ebrych@gmail.com>
2018-10-10 09:55:48 -0500

    Update reservas.component.ts

--- src/app/components/reservas/reservas.component.ts
+++ src/app/components/reservas/reservas.component.ts
@@ -19,15 +19,43 @@ export class ReservasComponent implements OnInit {
   id=localStorage.getItem('id');
   token=localStorage.getItem('token');
   ngOnInit() {
+    listaReservas(this.id,this.token)
   }
   
-  listaReservas(){
+  listaReservas(id,token){
+    let body = new URLSearchParams();
+      body.set('id', id);
+      body.set('token', token);
+    let options = {
+        headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    };
+    const req=this.http.post(this.baseApiUrl+'Reservas/listarReservas',body.toString(), options)
+      .subscribe(res=>{ this.groupList= res; },
+      err=>{console.log("Error ocurred")});
   }
   
   acepta(idRe){
+    let body = new URLSearchParams();
+      body.set('id', id);
+      body.set('token', token);
+    let options = {
+        headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    };
+    const req=this.http.post(this.baseApiUrl+'Reservas/confirmReserva',body.toString(), options)
+      .subscribe(res=>{ this.groupList= res; },
+      err=>{console.log("Error ocurred")});
   }
   
   rechaza(idRe){
+    let body = new URLSearchParams();
+      body.set('id', id);
+      body.set('token', token);
+    let options = {
+        headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    };
+    const req=this.http.post(this.baseApiUrl+'Reservas/cancelaReserva',body.toString(), options)
+      .subscribe(res=>{ this.groupList= res; },
+      err=>{console.log("Error ocurred")});
   }
   
   
